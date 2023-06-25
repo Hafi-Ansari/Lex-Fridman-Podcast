d6cf62d5f61d2c51d6078873eb02942743af82bf
hafi-ansari <hafi.m.ansari@gmail.com>
2023-06-25 11:04:17 -0500

    environment variables and twitter tokens

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
+const Twit = require('twit');
+
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET_KEY,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+// Make Twitter API calls using T object
 
 const port = 5000;
 app.listen(port, () => {
