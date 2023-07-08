cf864c943cce4291aff73bd29c62c545ffd49a05
hafi-ansari <hafi.m.ansari@gmail.com>
2023-07-08 11:14:16 -0500

    Added modal to search result selected

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,5 +1,33 @@
 import React, { useState } from "react";
 
+// Simple modal component
+const Modal = ({ onClose, children, title, date }) => {
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto ">
+      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 bg-black opacity-40"></div>
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
+          {" "}
+          &#8203;
+        </span>
+        <div className="inline-block align-bottom bg-midGreen rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              className="p-2 rounded-xl w-21 mb-2 bg-midAccentGreen hover:opacity-50"
+              onClick={onClose}
+            >
+              {title} - {date}
+            </button>
+          </div>
+          <div className="p-4 text-black overflow-auto max-h-screen">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Search = ({ title, transcript, date, query }) => {
   const [showFullTranscript, setShowFullTranscript] = useState(false);
 
@@ -48,7 +76,11 @@ const Search = ({ title, transcript, date, query }) => {
           {afterQuery}
         </span>
       )}
-      {showFullTranscript && displayedTranscript}
+      {showFullTranscript && (
+        <Modal onClose={toggleShowFullTranscript} title={title} date={date}>
+          {displayedTranscript}
+        </Modal>
+      )}
     </div>
   );
 };
